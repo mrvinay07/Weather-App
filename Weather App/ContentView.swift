7b17a9ef8ec78222a2242220039f3d0ee923e0be
Vinay Sharma <sharmavinay2108@gmail.com>
2024-11-28 09:38:40 +0530

    Initial Commit

---
+++ Weather App/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Weather App
+//
+//  Created by Vinay Sharma on 28/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        ZStack{
+            LinearGradient(gradient:Gradient(colors:[.blue,.white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                Text("Delhi India")
+                    .font(.system(size: 40,weight: .medium,
+                                  design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                    .strikethrough()
+                VStack(spacing:10){
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 160)
+                    Text("30°")
+                        .font(.system(size: 70,weight: .medium))
+                        .foregroundColor(.white)
+                    
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Vinay Sharma on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
