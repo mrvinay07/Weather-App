147f225cfafa97da65dde5fe5807519f6099f483
Vinay Sharma <sharmavinay2108@gmail.com>
2024-12-14 12:26:15 +0530

    "check chnages"

--- Weather App/ContentView.swift
+++ Weather App/ContentView.swift
@@ -35,6 +35,9 @@ struct ContentView: View {
                     Text("30°")
                         .font(.system(size: 70,weight: .medium))
                         .foregroundColor(.white)
+                    Text("30°")
+                        .font(.system(size: 70,weight: .medium))
+                        .foregroundColor(.white)
                     
                 }
                 Spacer()
